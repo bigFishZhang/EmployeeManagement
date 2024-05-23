dbf8aa6ce14530eab455078182e5e0a5b1b52c7f
leozbzhang <leozbzhang@tencent.com>
2024-05-23 23:59:52 +0800

    login vc

--- Employees/Employees/Login/VC/LoginViewController.m
+++ Employees/Employees/Login/VC/LoginViewController.m
@@ -9,34 +9,74 @@
 
 @interface LoginViewController ()
 
-@property (nonatomic, strong) UITextField *nameTextView;
+@property (nonatomic, strong) UILabel *bUesrName;
+@property (nonatomic, strong) UITextField *userName;
+@property (nonatomic, strong) UILabel *bUserKey;
+@property (nonatomic, strong) UITextField *userKey;
 
-@property (nonatomic, strong) UITextField *pastWordTextView;
-
+@property (nonatomic, strong) UIButton *login;
+@property (nonatomic, strong) UIButton *registerBtn;
 @end
 
 @implementation LoginViewController
 
-- (void)viewDidLoad 
+- (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
-    CGFloat screenWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    self.navigationItem.title = @"注册/登录";
     
-    UILabel *mainLabel = [[UILabel alloc] initWithFrame: CGRectMake(20, 100, screenWidth - 40, 30)];
-    mainLabel.textColor = [UIColor whiteColor];
-    mainLabel.textAlignment = NSTextAlignmentCenter;
-    mainLabel.font = [UIFont systemFontOfSize:22 weight:UIFontWeightRegular];
-    mainLabel.text = @"注册/登录";
-    [self.view addSubview:mainLabel];
-    // 输入框
-//    self.nameTextView = [[UITextField alloc] initWithFrame:CGRectMake(40, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)];
     
-    // 按钮
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    _bUesrName = [[UILabel alloc] initWithFrame: CGRectMake(50, 100, 300, 70)];
+    _bUesrName.text = @"用户名：";
+    [self.view addSubview: _bUesrName];
+    
+    _userName = [[UITextField alloc] initWithFrame: CGRectMake(50, 150, 300, 50)];
+    _userName.borderStyle = UITextBorderStyleLine;
+    _userName.placeholder = @"请输入用户名......";
+    [self.view addSubview: _userName];
+    
+    _bUserKey = [[UILabel alloc] initWithFrame: CGRectMake(50, 200, 300, 70)];
+    _bUserKey.text = @"密码:";
+    [self.view addSubview: _bUserKey];
+    
+    _userKey = [[UITextField alloc] initWithFrame: CGRectMake(50, 250, 300, 50)];
+    _userKey.borderStyle = UITextBorderStyleLine;
+    _userKey.keyboardType = UIKeyboardTypePhonePad;
+    _userKey.placeholder = @"请输入密码......";
+    [self.view addSubview: _userKey];
+    
+    _login = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+    _login.frame = CGRectMake(100, 350, 100, 60);
+    _login.backgroundColor = [UIColor brownColor];
+    [_login setTitle: @"登陆" forState: UIControlStateNormal];
+    [_login setTitleColor: [UIColor blueColor] forState: UIControlStateNormal];
+    [_login addTarget: self action: @selector(pressLogin) forControlEvents: UIControlEventTouchUpInside];
+    [self.view addSubview: _login];
+    
+    
+    _registerBtn = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+    _registerBtn.frame = CGRectMake(250, 350, 100, 60);
+    _registerBtn.backgroundColor = [UIColor brownColor];
+    [_registerBtn setTitle: @"注册" forState: UIControlStateNormal];
+    [_registerBtn setTitleColor: [UIColor blueColor] forState: UIControlStateNormal];
+    [_registerBtn addTarget: self action: @selector(pressRegister) forControlEvents: UIControlEventTouchUpInside];
+    [self.view addSubview: _registerBtn];
     
     
 }
 
 
+
+- (void)pressLogin
+{
+    NSLog(@"pressLogin");
+}
+
+- (void)pressRegister
+{
+    NSLog(@"pressLogin");
+}
 
 @end
